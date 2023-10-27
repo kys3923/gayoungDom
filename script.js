6881eaa997f41f2423ab38331655749b3ba7a322
YK <kys3923@gmail.com>
2023-10-27 10:46:33 -0400

    'lectureDay1'

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('yay')
+
+// eventLister
+
+document.addEventListener('DOMContentLoaded', (e) => {
+  console.log('yay1')
+  
+  let button1 = document.getElementById("submit")
+  let name = document.getElementById('name')
+
+  button1.addEventListener('click', (e) => {
+    e.preventDefault(); //refresh를 방지함
+    console.log(name.cehcked, 'button clicked')
+  })
+
+})
+
+// id => document.getElementByID('asdfasdf')
+// class => document.getElentByClassName('asdfasdf')
+// query => document.querySelector('aadsf')
+// query => document.querySelectorAll('p')
